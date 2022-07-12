cd50c5d2ebd098d9abb370ef1b426a6f36e6249c
miloszKubiak <milypan69@gmail.com>
2022-07-12 11:44:56 +0200

    setup project

---
+++ src/Modal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Modal = () => {
+	return <h2>modal component</h2>;
+};
+
+export default Modal;
---
+++ src/SetupForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SetupForm = () => {
+	return <h2>setup form</h2>;
+};
+
+export default SetupForm;
